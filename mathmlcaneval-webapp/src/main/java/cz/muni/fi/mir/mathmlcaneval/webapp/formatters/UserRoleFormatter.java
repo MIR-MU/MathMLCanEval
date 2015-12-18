06199b3c7f5fad2528c10d1ae428a8bca5ec5e3d
Dominik Szalai <emptulik@gmail.com>
2015-12-18 13:15:15 +0100

    changes making webapp runnable

---
+++ mathmlcaneval-webapp/src/main/java/cz/muni/fi/mir/mathmlcaneval/webapp/formatters/UserRoleFormatter.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2015 MIR@MU.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.muni.fi.mir.mathmlcaneval.webapp.formatters;
+
+import cz.muni.fi.mir.mathmlcaneval.api.UserRoleService;
+import cz.muni.fi.mir.mathmlcaneval.services.Mapper;
+import cz.muni.fi.mir.mathmlcaneval.webapp.forms.UserRoleForm;
+import java.text.ParseException;
+import java.util.Locale;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.Formatter;
+
+/**
+ *
+ * @author Dominik Szalai - emptulik at gmail.com
+ */
+public class UserRoleFormatter implements Formatter<UserRoleForm>
+{
+    @Autowired
+    private UserRoleService userRoleService;
+    @Autowired
+    private Mapper mapper;
+    
+    @Override
+    public String print(UserRoleForm t, Locale locale)
+    {
+        return t.getId() != null ? t.getId().toString() : null;
+    }
+
+    @Override
+    public UserRoleForm parse(String string, Locale locale) throws ParseException
+    {
+        if(StringUtils.isEmpty(string))
+        {
+            throw new ParseException("Given text is empty", 0);
+        }
+        
+        return mapper.map(userRoleService.getByID(Long.valueOf(string)), UserRoleForm.class);
+    }
+    
+}
---
+++ mathmlcaneval-webapp/src/main/java/cz/muni/fi/mir/mathmlcaneval/webapp/forms/UserRoleForm.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright 2015 MIR@MU.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package cz.muni.fi.mir.mathmlcaneval.webapp.forms;
+
+import java.util.Objects;
+
+/**
+ *
+ * @author Dominik Szalai - emptulik at gmail.com
+ */
+public class UserRoleForm
+{
+    private Long id;
+    private String roleName;
+
+    public Long getId()
+    {
+        return id;
+    }
+
+    public void setId(Long id)
+    {
+        this.id = id;
+    }
+
+    public String getRoleName()
+    {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName)
+    {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "UserRoleForm{" + "id=" + id + ", roleName=" + roleName + '}';
+    }
+
+    @Override
+    public int hashCode()
+    {
+        int hash = 7;
+        hash = 89 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (obj == null)
+        {
+            return false;
+        }
+        if (getClass() != obj.getClass())
+        {
+            return false;
+        }
+        final UserRoleForm other = (UserRoleForm) obj;
+        return Objects.equals(this.id, other.id);
+    }    
+}
